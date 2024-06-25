029878b5f71b7c3b75cdd052b1ce19fd8c57f87d
José <128116145+JjJjJose@users.noreply.github.com>
2024-06-25 02:23:36 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,5 +3,31 @@ import matplotlib.pyplot as plt
 
 df = pd.read_csv("games.csv")
 
-df.plot.hist(column="white_rating")
+
+# df.plot.hist(column="white_rating")
+# plt.show()
+
+
+# numero de vitorias
+#victory_counts = df['victory_status'].value_counts()
+#victory_counts.plot(kind='bar')
+#plt.xlabel('victory status')
+#plt.ylabel('number of games')
+#plt.title('distribuição de status de vitoria')
+
+## aberturas
+#opening_counts = df['opening_eco'].value_counts()
+#opening_counts.plot(kind='bar')
+#plt.xlabel('opening code')
+#plt.ylabel('number of games')
+#plt.title('popularidade de diferentes aberturas')
+
+# comparação de rating
+# plt.scatter(df['white_rating'], df['black_rating'])
+# plt.xlabel('white rating')
+# plt.ylabel('black rating')
+# plt.title('pontuação de brancas vs pretas')
+
+
+
 plt.show()
